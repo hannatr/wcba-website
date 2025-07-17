36311a88719722b8134bbb9395f422c3a2c4d6eb
user <hannatr@gmail.com>
2025-07-17 16:02:53 -0400

    Turn on analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 import "@/app/globals.css";
 
 import Footer from "@/components/Footer";
@@ -23,6 +24,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <Analytics/>
       </body>
     </html>
   );
